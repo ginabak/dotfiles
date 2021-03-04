56d9a90ecae9a286d855dd473b47c1999acba350
Gina <43223543+ginabak@users.noreply.github.com>
2021-03-04 12:00:28 -0500

    Update aliases.zsh

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -1,3 +1,8 @@
 # Local aliases
 # --------------------------------------
 
+alias gpom="git pull origin master"
+alias gpom1="git pull origin main"
+alias gfap="git fetch --all --prune"
+alias glg="git log --oneline --abbrev-commit"
+alias gcf="git commit --fixup"
